531025709c4eaa19ada74f680d61cd69e1cdd6d1
venaraya <venkata.eepuri@gmail.com>
2024-05-15 16:23:15 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,9 +22,6 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 pd_df = my_dataframe.to_pandas()
-#st.dataframe(pd_df)
-#st.stop()
-
 
 ingredients_list = st.multiselect('Choose Up to 5 ingredients:', my_dataframe, max_selections=5)
 
