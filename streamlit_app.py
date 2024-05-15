54b49b4bb1c99eb9c8076719f4ec2a361314c65a
venaraya <venkata.eepuri@gmail.com>
2024-05-15 16:11:25 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,11 @@ st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
+search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
 ingredients_list = st.multiselect('Choose Up to 5 ingredients:', my_dataframe, max_selections=5)
 
 if ingredients_list:
