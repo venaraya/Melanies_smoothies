6513cd4f97d50cf335a7574f58239e1adeb1dd12
venaraya <venkata.eepuri@gmail.com>
2024-05-15 16:32:02 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,7 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'));
 st.dataframe(data=my_dataframe, use_container_width=True)
 st.stop()
-
+# Convert the Snowpark dataframe into Pandas dataframe so that we can use in loc 
 pd_df = my_dataframe.to_pandas()
 
 ingredients_list = st.multiselect('Choose Up to 5 ingredients:', my_dataframe, max_selections=5)
