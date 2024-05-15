2f7942fdf52565a4a8cb1c1bdba8113c6a79670f
venaraya <venkata.eepuri@gmail.com>
2024-05-15 16:09:53 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import streamlit as st
 # from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
-import pandas
+import pandas as pd
 
 
 # Write directly to the app
